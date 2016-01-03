94b6e163539affcca8680e45d0a9c0024b94a6d5
pemrouz <p3pedram@hotmail.com>
2016-01-03 21:28:45 +0000

    graceful return

--- test.js
+++ test.js
@@ -2,7 +2,13 @@ var expect = require('chai').expect
   , first = require('./')
   
 describe('first', function() {
+
   it('should return first element', function() {
     expect(first(['a', 'b'])).to.equal('a')
   })
+
+  it('should be graceful', function() {
+    expect(first()).to.equal(undefined)
+  })
+
 })
